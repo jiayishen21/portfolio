d86000c40a79bed72e9b4bb4b1c89242eb935e1d
jiayishen21 <lucasshen21@gmail.com>
2023-08-02 19:55:41 +0800

    project setup

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Project: React.FC = () => {
+  return (
+    <>
+      <nav>
+
+      </nav> 
+    </>
+  )
+}
+
+export default Project
---
+++ src/components/Project.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+interface Props {
+  name: string;
+  link: string;
+}
+
+const Project: React.FC<Props> = (props) => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Project
